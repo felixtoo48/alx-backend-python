c3552dc3a7d50a455ab10086a8d31578d1bb3865
felixtoo48 <felixtoo48@gmail.com>
2023-10-06 05:29:17 +0300

    function returning string represenatation of a float n

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function returning string repr of a float n
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float number to its string representation.
+
+    :param n: The input float number.
+    :return: The string representation of the input float.
+    """
+    return str(n)
