fd49a7ccc7636a921d88e50f54368eafd74420de
felixtoo48 <felixtoo48@gmail.com>
2023-10-06 06:04:39 +0300

    error

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,18 +1,8 @@
 #!/usr/bin/env python3
-"""
-function’s parameters and return values with the appropriate types
-"""
-from typing import List, Tuple
+"""Annonated function, returns values with appropriate types"""
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
-    """
-    Calculates the length of each element in the input list and
-    returns a list of tuples.
-
-    :param lst: The input list of strings.
-    :return: A list of tuples, where each tuple contains a
-    string from the input list
-             and its corresponding length as an integer.
-    """
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returns a list of tuples with lenth of each"""
     return [(i, len(i)) for i in lst]
