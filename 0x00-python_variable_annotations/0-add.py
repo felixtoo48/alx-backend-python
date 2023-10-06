2676e89232b8e4f9ef39e62c3ba8590a26572295
felixtoo48 <felixtoo48@gmail.com>
2023-10-06 05:08:02 +0300

    add fload numbers given and return a float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+add given parameters
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns their sum.
+    
+    :param a: The first float number.
+    :param b: The second float number.
+    :return: The sum of a and b as a float.
+    """
+    return a + b
+
