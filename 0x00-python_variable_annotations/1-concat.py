bf858c511c05b9094015bdf57ee2fc927bfe5ebb
felixtoo48 <felixtoo48@gmail.com>
2023-10-06 05:16:53 +0300

    concatenate strings str1 and str2 given

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+concat strings str1 and str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two input strings and returns the result.
+
+    :param str1: The first string.
+    :param str2: The second string.
+    :return: The concatenated string.
+    """
+    return str1 + str2
