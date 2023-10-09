15311198f98bbac7872cca1bc3aa4cfb0273e886
felixtoo48 <felixtoo48@gmail.com>
2023-10-09 22:21:51 +0300

    update error

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,11 +9,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int = 10) -> float:
-    """
-    Function with integer arguments n and max_delay.
-    Measures total execution time for wait_n.
-    Returns total_time / n.
-    """
+    """ measure runtime  """
     start_time = time.time()  # Record the start time
 
     # Measure the total execution time for wait_n(n, max_delay)
