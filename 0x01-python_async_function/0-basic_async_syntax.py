c16f3b35701172879bfe2a833c75f3b96ddf6c53
felixtoo48 <felixtoo48@gmail.com>
2023-10-09 19:48:01 +0300

    adding comment after def function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -10,10 +10,14 @@ from numpy import random
 
 
 async def wait_random(max_delay: int=10) -> float:
-    # Generate a random delay between 0 and max_delay seconds (inclusive).
+    """
+    Generate a random delay between 0 and max_delay seconds (inclusive).
+    """
     random_delay: float = random.uniform(0, max_delay)
 
-    # wait for random delay
+    """
+    wait for random delay
+    """
     await asyncio.sleep(random_delay)
 
     return random_delay
