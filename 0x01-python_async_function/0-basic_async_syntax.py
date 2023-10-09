095f76770f2681f07555891ce367f237a592c7df
felixtoo48 <felixtoo48@gmail.com>
2023-10-09 20:17:18 +0300

    updating errors

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-"""
-asynchronous coroutine taking in an integer argument(max delay) with
-default value of 10
-named (wait_random) that awaits a delay between 0 and 10(max delay)
-wait random value includes float values
-"""
+"""an asynchronous coroutine that takes in an integer argument"""
 import asyncio
-from numpy import random
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
@@ -15,8 +10,6 @@ async def wait_random(max_delay: int = 10) -> float:
     """
     random_delay: float = random.uniform(0, max_delay)
 
-    """
-    wait for random delay
-    """
+    """ wait for random delay """
     await asyncio.sleep(random_delay)
     return random_delay
