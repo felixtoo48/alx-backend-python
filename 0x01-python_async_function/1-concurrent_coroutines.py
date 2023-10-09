161e807b799e560acba48d98b98849466813f5df
felixtoo48 <felixtoo48@gmail.com>
2023-10-09 20:49:09 +0300

    removing sort function and adding append (the generated random list in order

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,13 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Spawn wait_random n times with the specified max_delay.
     Return a list of delays in ascending order.
     """
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    delays = await asyncio.gather(*tasks)
-    # delays.sort()  # Sort the list of delays in ascending order
+    delays = []
+
+    async def wait_and_append_delay():
+        delay = await wait_random(max_delay)
+        delays.append(delay)
+
+    tasks = [wait_and_append_delay() for _ in range(n)]
+    await asyncio.gather(*tasks)
+
     return delays
