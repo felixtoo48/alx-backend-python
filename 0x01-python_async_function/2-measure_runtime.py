4490059a66f1cdcab502a164bed762225aca0a7d
felixtoo48 <felixtoo48@gmail.com>
2023-10-09 21:45:16 +0300

    printing result error

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -14,12 +14,12 @@ async def measure_time(n: int, max_delay: int = 10) -> float:
     measures total execution time for wait_n
     return: total_time / n
     """
-    start_time = time.time()  # Record the start time
+    start_time = time.perf_counter()  # Record the start time
 
     # Measure the total execution time for wait_n(n, max_delay)
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
 
-    end_time = time.time()  # Record the end time
+    end_time = time.perf_counter()  # Record the end time
     total_time = end_time - start_time
 
     # Calculate and return the average time per coroutine execution
