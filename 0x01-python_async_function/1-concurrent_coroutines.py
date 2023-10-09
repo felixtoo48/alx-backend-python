a9d4d9b9879360b252d0da21c8949a30784cc961
felixtoo48 <felixtoo48@gmail.com>
2023-10-09 20:40:24 +0300

    executing multiple coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""an asynchronous coroutine that takes 2 arguments"""
+import random
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawn wait_random n times with the specified max_delay.
+    Return a list of delays in ascending order.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    delays.sort()  # Sort the list of delays in ascending order
+    return delays
