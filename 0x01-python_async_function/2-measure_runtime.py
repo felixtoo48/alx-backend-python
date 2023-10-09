ee93273e2817e5d52ce89272ffed05c1a1c86073
felixtoo48 <felixtoo48@gmail.com>
2023-10-09 22:10:40 +0300

    removing white space

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,6 +7,7 @@ import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 async def measure_time(n: int, max_delay: int = 10) -> float:
     """
     Function with integer arguments n and max_delay.
